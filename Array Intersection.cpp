472cfca4ba32763d0e377fdb82c535c94c67f4a7
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2022-02-26 20:13:57 +0530

    Create Array Intersection.cpp

---
+++ Array Intersection.cpp
@@ -0,0 +1,43 @@
+Array Intersection
+You have been given two integer arrays/list(ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
+Note :
+Input arrays/lists can contain duplicate elements.
+
+The intersection elements printed would be in the order they appear in the first sorted array/list(ARR1).
+
+
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+The second line contains 'N' single space separated integers representing the elements of the first the array/list.
+
+The third line contains an integer 'M' representing the size of the second array/list.
+
+The fourth line contains 'M' single space separated integers representing the elements of the second array/list.
+Output format :
+For each test case, print the intersection elements in a row, separated by a single space.
+
+Output for every test case will be printed in a separate line.
+  // solution
+  void intersection(int *arr1, int *arr2, int n, int m) 
+{
+    //Write your code here
+    sort(arr1,arr1+n);
+    sort(arr2,arr2+m);
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j])
+            i++;
+        else if (arr2[j] < arr1[i])
+            j++;
+        else /* if arr1[i] == arr2[j] */
+        {
+            cout << arr2[j] << " ";
+            i++;
+            j++;
+        }
+    }
+    
+}
